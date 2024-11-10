793e770ac6b8348d073c4ad1f4f7dc5377222836
omribarouch <omrib@getport.io>
2024-11-10 13:41:19 +0200

    Migrate org_BneDtWovPqXaA2VZ to work with auto-sync

--- migrate_organizations.sh
+++ migrate_organizations.sh
@@ -15,7 +15,7 @@ for dir in "$PARENT_DIR"/*/; do
 
         node change_gitops.js "$dir"
 
-        if [[ $(git status --porcelain) ]]; then
+        if [[ $(git ls-files --modified '*.yml') ]]; then
             echo "Changes detected in $dir, committing changes..."
             APP_NAME=$(basename "$dir")
 
@@ -23,8 +23,8 @@ for dir in "$PARENT_DIR"/*/; do
             git commit -m "Migrate $APP_NAME to work with auto-sync"
             git push origin main
 
-            echo "Refreshing and syncing ArgoCD application: $APP_NAME"
-            curl -X GET "${ARGO_URL}/api/v1/applications/${APP_NAME}?refresh=hard" \
+            echo "Refreshing and syncing ArgoCD org application: $APP_NAME"
+            curl -X GET "${ARGO_URL}/api/v1/applications/${APP_NAME}?refresh=hard&appNamespace=$APP_NAME&project=ocean" \
                 -H "Authorization: Bearer ${ARGO_TOKEN}" \
                 -H "Content-Type: application/json"
 

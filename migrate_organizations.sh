cfa3b875c1927357422b090650dc9cda44b80d41
omribarouch <omrib@getport.io>
2024-11-10 14:00:17 +0200

    finished migration

--- migrate_organizations.sh
+++ migrate_organizations.sh
@@ -15,11 +15,12 @@ for dir in "$PARENT_DIR"/*/; do
 
         node change_gitops.js "$dir"
 
-        if [[ $(git ls-files --modified '*.yml') ]]; then
+        CHANGED_YML_FILES=$(git ls-files --modified '*.yml')
+        if [[ -n "${CHANGED_YML_FILES}" ]]; then
             echo "Changes detected in $dir, committing changes..."
             APP_NAME=$(basename "$dir" | tr '[:upper:]' '[:lower:]' | tr '_' '-')
 
-            git add "$PARENT_DIR/$dir"
+            git add "$CHANGED_YML_FILES"
             git commit -m "Migrate $APP_NAME to work with auto-sync"
             git push origin main
 
